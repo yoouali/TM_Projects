dec9b76499727091a5cd378644a3fe8365245fcb
unknown <youssefoualifessuoy@gmail.com>
2021-08-14 23:22:06 +0200

    loading blur

--- blurry_loading/script.js
+++ blurry_loading/script.js
@@ -5,6 +5,7 @@ let = Loading = 0
 
 let int = setInterval(Bluring, 30)
 
+
 function Bluring() {
     Loading++
 
@@ -14,9 +15,11 @@ function Bluring() {
     }
     BlurPer.innerText = `${Loading}%`
     BlurPer.style.opacity = scale(Loading, 0, 100, 1, 0)
-    console.log(Loading)
+
+    BackGround.style.filter = `blur(${scale(Loading, 0, 100, 30, 0)}px)`
 }
 
+//map a range of numbers to another range of numbers
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
